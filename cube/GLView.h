49e1ac57cbd4f65c327c3fbe07b3080c29fbcd4a
tzviki fisher <tzvikif@gmail.com>
2013-07-17 15:50:35 +0300

    remove excessive lines from GLView

--- cube/GLView.h
+++ cube/GLView.h
@@ -10,8 +10,6 @@
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
 #import <QuartzCore/QuartzCore.h>
-#include "ApplicatonEngine.hpp"
-#import "LoadObj.h"
 
 
 @interface GLView : UIView
@@ -21,45 +19,12 @@
     
     GLuint _colorRenderBuffer;
     GLuint _depthRenderBuffer;
-//    GLuint _positionSlot;
-//    GLuint _colorSlot;
-//    GLuint _projectionUniform;
-//    GLuint _modelViewUniform;
-//    
-//    GLuint _vertexBuffer;
-//    GLuint _indexBuffer;
-//    float _currentRotataion;
-//    ApplicationEngine *ap;
-//    float m_factor;
-//    GLint _uniform_fade;
-//    CFTimeInterval _timeSinceLastUpdate;
-//    CFTimeInterval _timeRotation;
-//    float _rotationAngle;
-//    GLuint vbo_cube_vertices, vbo_cube_colors, vbo_cube_texcoords;
-//    GLuint ibo_cube_elements;
-//    GLuint _texture_id;
-//    GLint _uniform_mytexture;
-//    GLint _attribute_texcoord;
+
 }
 + (Class)layerClass;
 - (void)setupLayer;
 - (void)setupContext;
 - (void)setupRenderBuffer;
 - (void)setupFrameBuffer;
-//- (void)render:(CADisplayLink*)displayLink;
-//- (GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType;
-//- (void)compileShaders;
-//- (void)setupVBOs;
-//- (void)setupDisplayLink;
-//- (void)setupDepthBuffer;
-//- (void)handlePinchGesture:(UIGestureRecognizer *)sender;
-//- (void)createGestureRecognizers;
-//- (void)update:(CADisplayLink*)displayLink;
-//- (GLuint)setupTexture:(NSString *)fileName;
-//- (void)setupTextures;
-//- (void)initResources;
-//- (void)loadObj;
 - (void)presentRenderbuffer;
-//-(void)UpdateAnimation:(float)dt;
-@property(retain,nonatomic) LoadObj *objLoader;
 @end
